603f2acac71bb614fffa81e590401f9213eb5638
Maximilien ZAKOWSKI <maximilien.zakowski@gmail.com>
2019-10-16 21:41:56 +0200

    First Commit
    Project initialization

---
+++ DBTemplateHandler.Core/TemplateHandlers/AbstractTemplateContextHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DBTemplateHandler.Core.TemplateHandlers
+{
+    public abstract class AbstractTemplateContextHandler
+    {
+        public abstract string ContextStart { get; }
+        public abstract string ContextEnd { get; }
+        public abstract bool IsContextAnEntireWord { get; }
+        public string Signature { get { return $"{ContextStart}{ContextEnd}"; } }
+    }
+}
