398c31359214c06945691acbd95bc6eba0ccb732
cosXsinX <maximilien.zakowski@gmail.com>
2019-11-24 11:59:53 +0100

    DBTemplateHandler.DatabaseModel.Import.IntegrationTests project add

---
+++ DBTemplateHandler.DatabaseModel.Import.IntegrationTests/SQLServerImporterIntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using DBTemplateHander.DatabaseModel.Import.Importer;
+using NUnit.Framework;
+using System;
+
+namespace DBTemplateHandler.DatabaseModel.Import.IntegrationTests
+{
+    [TestFixture]
+    public class SQLServerImporterIntegrationTest
+    {
+        public SQLServerImporter _tested;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _tested = new SQLServerImporter();
+        }
+
+        [Test]
+        public void ImportShouldNotFail()
+        {
+            string connectionString = "Data Source=DESKTOP-JNFJSV9\\SQLEXPRESS01;Initial Catalog=AdventureWorks2017";
+            _tested.Import(connectionString);
+        }
+    }
+}
