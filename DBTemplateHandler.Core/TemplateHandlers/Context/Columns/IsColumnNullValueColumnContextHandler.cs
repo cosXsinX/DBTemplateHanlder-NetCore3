bbfce842c1ae73de2bbd18dd93d9bcd5ee9e69a5
cosXsinX <maximilien.zakowski@gmail.com>
2019-12-15 22:05:16 +0100

    - C# Template enhancement - Add of column related additional semantic - Importer bug correction - TypeSet C# DBGeography type add - TypeSet Sql Server 2016 geography type add - SqlServer2016ToC# geography to DBGeography mapping item add - AdventureWorks2017 database duplicate columns removal

--- DBTemplateHandler.Core/TemplateHandlers/Context/Columns/IsColumnNullValueColumnContextHandler.cs
+++ DBTemplateHandler.Core/TemplateHandlers/Context/Columns/IsColumnNullValueColumnContextHandler.cs
@@ -5,20 +5,20 @@ using System;
 
 namespace DBTemplateHandler.Core.TemplateHandlers.Context.Columns
 {
-    public class IsColumnNotNullValueColumnContextHandler : AbstractColumnTemplateContextHandler
+    public class IsColumnNullValueColumnContextHandler : AbstractColumnTemplateContextHandler
     {
-        public IsColumnNotNullValueColumnContextHandler(TemplateHandlerNew templateHandlerNew) : base(templateHandlerNew) { }
-        public override string StartContext => "{:TDB:TABLE:COLUMN:FOREACH:CURRENT:IS:NOT:NULL(";
+        public IsColumnNullValueColumnContextHandler(TemplateHandlerNew templateHandlerNew) : base(templateHandlerNew) { }
+        public override string StartContext => "{:TDB:TABLE:COLUMN:FOREACH:CURRENT:IS:NULL(";
         public override string EndContext => "):::}";
         public override bool isStartContextAndEndContextAnEntireWord => false;
-        public override string ContextActionDescription => "Is replaced by the inner context when the current iteration column is not a nullable value column";
+        public override string ContextActionDescription => "Is replaced by the inner context when the current iteration column is a nullable value column";
 
 
         public override string processContext(string StringContext)
         {
             ControlContext(StringContext);
             string TrimedStringContext = TrimContextFromContextWrapper(StringContext);
-            if (ColumnModel.IsNotNull)
+            if (!ColumnModel.IsNotNull)
             {
                 return HandleTrimedContext(TrimedStringContext);
             }
