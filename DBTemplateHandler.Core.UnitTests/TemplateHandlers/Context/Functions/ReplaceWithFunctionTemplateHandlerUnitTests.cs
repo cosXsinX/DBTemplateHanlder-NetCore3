2b03e377703534e54c44717a8c649f14741b2a57
cosXsinX <maximilien.zakowski@gmail.com>
2019-12-23 09:41:18 +0100

    Replace With function implementation begining

---
+++ DBTemplateHandler.Core.UnitTests/TemplateHandlers/Context/Functions/ReplaceWithFunctionTemplateHandlerUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using DBTemplateHandler.Core.TemplateHandlers.Context.Functions;
+using DBTemplateHandler.Core.TemplateHandlers.Handlers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBTemplateHandler.Core.UnitTests.TemplateHandlers.Context.Functions
+{
+    [TestFixture]
+    public class ReplaceWithFunctionTemplateHandlerUnitTests
+    {
+        private ReplaceWithFunctionTemplateHandler _tested;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var templateHandlerNew = new TemplateHandlerNew(null);
+            _tested = new ReplaceWithFunctionTemplateHandler(templateHandlerNew);
+        }
+
+        [Test]
+        public void ShouldThrowAnArgumentNullExpcetionWhenStringContextIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => _tested.processContext(null));
+        }
+
+        //TODO implement missing Unit tests
+    }
+}
---
+++ DBTemplateHandler.Core/TemplateHandlers/Context/Functions/ReplaceWithFunctionTemplateHandler.cs
@@ -0,0 +1,32 @@
+﻿using DBTemplateHandler.Core.TemplateHandlers.Handlers;
+using System;
+
+namespace DBTemplateHandler.Core.TemplateHandlers.Context.Functions
+{
+    public class ReplaceWithFunctionTemplateHandler : AbstractFunctionTemplateContextHandler
+    {
+
+        public ReplaceWithFunctionTemplateHandler(TemplateHandlerNew templateHandlerNew) : base(templateHandlerNew) { }
+        public override string StartContext { get => "{:TDB:FUNCTION:REPLACE("; }
+        public string WithSeparator { get => "<-:WITH:["; }
+        public override string EndContext { get => "])::}"; }
+        public override bool isStartContextAndEndContextAnEntireWord => false;
+        public override string ContextActionDescription => "Is replaced by the intern context with the first letter of intern context Uppercased";
+
+        public override string processContext(string StringContext)
+        {
+            if (StringContext == null)
+                throw new ArgumentNullException(nameof(StringContext));
+            string TrimedStringContext =
+                    TrimContextFromContextWrapper(StringContext);
+            //Function performed operation
+            if (TrimedStringContext.Equals("")) return TrimedStringContext;
+            var splitted = TrimedStringContext.Split(new string[] { WithSeparator },StringSplitOptions.None) ;
+            if (splitted.Length != 2) return TrimedStringContext;
+
+            var handledResult = HandleTrimedContext(splitted[0]);
+            var result = handledResult?.Replace(handledResult, splitted[1]);
+            return result;
+        }
+    }
+}
